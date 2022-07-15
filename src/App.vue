d1d341ecd2850bb8ef6d5236acd1b16165fd5abf
Neo-Machina <andreistatache1997@gmail.com>
2022-07-15 23:22:02 +0200

    alpha header-top

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-  
+    <HeaderComponent />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 
 export default {
   name: 'App',
   components: {
-
+    HeaderComponent
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './style/common.scss'
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <header>
+        <div class="container">
+            <!-- nav -->
+            <nav class="row align-items-center">
+                <div class="col">
+                    <img src="../assets/images/dark-logo.png" alt="logo">
+                </div>
+                
+                <!-- links-web-page -->
+                <div class="col">
+                    <ul class="nav_links d-flex justify-content-between">
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                    </ul>
+                </div>
+
+                <!-- icon-links -->
+                <div class="col">
+                    <ul class="nav_icon-links d-flex justify-content-end">
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    height: 70px;
+    background-color: lightblue;
+
+    .container { 
+        height: 100%;
+
+        nav {
+            height: 100%;
+            .col {
+                img {
+                    width: 8rem;
+                    
+                }   
+                
+                .nav_links {
+                    padding-left: 0;
+                }
+
+                .nav_icon-links{
+                    li {
+                        margin-left: 10px;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
